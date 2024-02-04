78d4a69a2a9c1277489dd2d684a24ac7f3d485ed
Churamani budhathoki <155580704+churamanib@users.noreply.github.com>
2024-02-04 23:19:27 +0530

    Update Linux_setup.sh

--- Linux_setup.sh
+++ Linux_setup.sh
@@ -7,18 +7,14 @@ BL='\e[34m'
 
 
 echo -e "
-write-host "  .d8888b.           888       .d8888b.               8888888888P                  888    " -foregroundcolor "Red"
-write-host " d88P  Y88b          888      d88P  Y88b                    d88P                   888    " -foregroundcolor "Red"
-write-host " 888    888          888           .d88P                   d88P                    888    " -foregroundcolor "Red"
-write-host " 888        888  888 88888b.      8888'  888d888          d88P    .d88b.  .d8888b  888888 " -foregroundcolor "Red"
-write-host " 888        888  888 888 '88b      'Y8b. 888P'           d88P    d8P  Y8b 88K      888    " -foregroundcolor "white"
-write-host " 888    888 888  888 888  888 888    888 888            d88P     88888888 'Y8888b. 888    " -foregroundcolor "white"
-write-host " Y88b  d88P Y88b 888 888 d88P Y88b  d88P 888           d88P      Y8b.          X88 Y88b.  " -foregroundcolor "white"
-write-host "  'Y8888P'   'Y88888 88888P'   'Y8888P'  888          d8888888888 'Y8888   88888P'  'Y888 " -foregroundcolor "green"
-write-host "                 888                                                                      " -foregroundcolor "green"
-write-host "            Y8b d88P                                                                      " -foregroundcolor "green"
-write-host "             'Y88P'   
-echo "  this is hacker im churamani but pepole call me using (amar)
+${OR}  66666  66     66  66        66  6666666   666          666      6        666       66  666666666        ${NL} 
+${OR} 66      66     66  66        66  66     6  66  66    66  66    66  66     66  6     66     66         ${NL}
+${WH}66       66 666 66  66        66  66    66  66     66     66   66    66    66    6   66     66        ${NL}
+${WH}66       66 666 66  66        66  66 666    66            66  6666666666   66     6  66     66         ${NL}  
+${GR} 66      66     66   66      66   66    66  66            66  66       66  66      6 66     66       ${NL}
+${GR}  66666  66     66    6666666      66    66 66            66 66         66 66       666  666666666 ${NL}
+"
+echo "  This script is made by CyberZest
 "
 
 # Check if Java JDK 19 is installed
